d8782cbf04197c35d36dd93148517d0f15fc7bd0
Proftvv <ozcanyilmazcelebi2016@gmail.com>
2025-12-12 11:25:44 +0300

    Updated test user: custom_id='root', password='2503'

--- migrations/002_create_test_user_2503.sql
+++ migrations/002_create_test_user_2503.sql
@@ -1,14 +1,14 @@
--- Create test user with custom_id = 2503
--- Username: test2503
+-- Create test user with custom_id = root
+-- Username: testroot
 -- Password: 2503
--- Custom ID: 2503
+-- Custom ID: root
 
 INSERT INTO users (username, password_hash, custom_id, created_at) 
-VALUES ('test2503', '$2a$10$IQHK6DKrOg/Zkg2t3PileOsQ8tOcw8lTfyc.XCAiVOTSfFq64tidi', '2503', NOW());
+VALUES ('testroot', '$2a$10$IQHK6DKrOg/Zkg2t3PileOsQ8tOcw8lTfyc.XCAiVOTSfFq64tidi', 'root', NOW());
 
 -- Verification query:
--- SELECT * FROM users WHERE custom_id = '2503';
+-- SELECT * FROM users WHERE custom_id = 'root';
 
 -- You can now login with:
--- - Identifier: test2503, Password: 2503
--- - Identifier: 2503, Password: 2503
+-- - Identifier: testroot, Password: 2503
+-- - Identifier: root, Password: 2503
